88e29f0b2d8c12f2666d9c4d60c26fd2808d0d94
vamsi <pvamsivs@gmail.com>
2025-08-16 11:28:39 +0530

    16-07

--- frontend/ai-interview-q/src/pages/Auth/Login.jsx
+++ frontend/ai-interview-q/src/pages/Auth/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { validateEmail } from '../../utils/helper';
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Input from '../../components/Inputs/Input';
@@ -10,6 +11,34 @@ const Login = ({setCurrentPage}) => {
 	const handleLogin = async (e) =>
 	{
 		e.preventDefault()
+		if(!validateEmail(email))
+		{
+			setError("Please enter a valid email address");
+			return;
+		}
+		if(!password || password.length < 8)
+		{
+			setError("Password must be at least 8 characters long");
+			return;	
+		}
+		setError("");
+		//Login Api Call
+		try 
+		{
+
+		}
+		catch (error) 
+		{
+			if(error.response && error.response.data && error.response.data.message)
+			{
+				setError(error.response.data.message);
+			}
+			else
+			{
+				setError("An unexpected error occurred");
+			}
+		}
+		
 	};
 	
 	return (
@@ -33,18 +62,20 @@ const Login = ({setCurrentPage}) => {
 		label = "password"
 		placeholder = "Min 8 characters"
 		type = "password"/>
-	  </form>
+	  
 	  {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
 	  <button type="submit" className="btn-primary">
 		Login
 	  </button>
+	  
 	  <p className="text-[13px] text-slate-800 text-center mt-3 ">
 		Don't have an account? {""}
-		<button className ="font-meduim text-orange-400 underline " 
+		<button className ="font-medium text-orange-400 underline " 
 		onClick={() => {setCurrentPage("signup")}}>
 			Signup
 			</button>
 	  </p>
+	  </form>
 	</div>
   )
 }
