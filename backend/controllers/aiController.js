bca164b1e3109b0182f1158a79af4525e18e31fa
vamsi <pvamsivs@gmail.com>
2025-08-20 15:23:28 +0530

    genarating ai question using api key

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -9,7 +9,7 @@ const generateInterviewQuestions = async (req,res)=>
 		const {role,experience,topicsToFocus,numberOfQuestions} = req.body;
 		if(!role || !experience || !topicsToFocus || !numberOfQuestions)
 		{
-			return res.ststus(400).json({message:"Missing required fields"})
+			return res.status(400).json({message:"Missing required fields"})
 		}
 		const prompt = questionAnswerPrompt(role, experience, topicsToFocus, numberOfQuestions)
 		const response = await ai.models.generateContent(
@@ -30,15 +30,33 @@ const generateInterviewQuestions = async (req,res)=>
 		res.status(500).json({success: false,message:"error in genarating questions"})
 	}
 }
-const generateConceptExplanation = (req,res)=>
+const generateConceptExplanation = async (req,res)=>
 {
 try
 	{
+        const {question}= req.body
+		if(!question)
+		{
+           return res.status(400).json({sucess:false,message:"missing Required field"})
+		}
+		const prompt  = conceptExplainPrompt(question)
+		const response = await ai.models.generateContent(
+			{
+				model : "gemini-2.0-flash-lite",
+				contents:prompt
+			}
+		)
+		let rawText = response.text
+		const cleanedText = rawText.replace(/^```json\s*/, "")
+		.replace(/```$/,"")
+		.trim();
+		const data = JSON.parse(cleanedText)
+		return res.status(200).json(data)
 
 	}
 	catch(error)
 	{
-		res.status(500).json({message:"Failed to genarate",error:error.massage})
+		res.status(500).json({message:"Failed to genarate",error:error.message})
 	}
 }
 module.exports ={generateConceptExplanation,generateInterviewQuestions}
