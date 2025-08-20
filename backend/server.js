0009746f94fe834203de25487fc5ba9485b8f478
vamsi <pvamsivs@gmail.com>
2025-08-20 16:18:12 +0530

    20-08

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use("/api/sessions",sessionRoutes)
 app.use("/api/questions",questionRoutes)
 
 app.use("/api/ai/generate-questions",protect,generateInterviewQuestions)
-app.use("/api/ai/genarated-explanation",protect,generateConceptExplanation)
+app.use("/api/ai/generate-explanation",protect,generateConceptExplanation)
 
 app.use("/uploads",express.static(Path.join(__dirname,"uploads")))
 //start server
